c1cba0bea036b48f16e767ee02828c41cbf296a4
DianaMagallanes <dianakarina2003@example.com>
2022-04-20 19:33:58 -0700

    Cambio para Requerimiento 2 Se revisa que dateCreated y lastUpdated no son indefinidos.

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,14 +3,14 @@ const User = require('./../../app/models/User')
 describe("Unit Tests for User class", () => {
 
     test('Create an User object',() => {
-    const user = new User(1,"DianaMagallanes", "Diana", "Bio", "dateCreated", "lastUpdated")
+    const user = new User(1,"DianaMagallanes", "Diana", "Bio")
 
     expect(user.id).toBe(1)
     expect(user.username).toBe("DianaMagallanes")
     expect(user.name).toBe("Diana")
     expect(user.bio).toBe("Bio")
-    expect(user.dateCreated).toBe("dateCreated")
-    expect(user.lastUpdated).toBe("lastUpdated")  
+    expect(user.dateCreated).not.toBeUndefined()
+    expect(user.lastUpdated).not.toBeUndefined()
         
     });
 })
