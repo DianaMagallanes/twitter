84458585052c0e0886b814ff7eb2d756ae33ec12
DianaMagallanes <dianakarina2003@example.com>
2022-04-20 20:23:53 -0700

    Correccion nombre de propiedad Se corrige a name

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -5,7 +5,7 @@ describe("Tests for UserService",()=> {
     test("1. Create a new user using the UserService",()=>{
         const user = UserService.create(1,"DianaMagallanes", "Diana")
         expect(user.username).toBe("DianaMagallanes")
-        expect(user.username).toBe("Diana")
+        expect(user.name).toBe("Diana")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     })
