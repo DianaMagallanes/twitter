5efec38562e57dbea0df8ebc8fb7b9674785966f
DianaMagallanes <dianakarina2003@example.com>
2022-04-21 22:08:18 -0700

    Modificando class para Requerimiento 4 Se corrigen lo que usaba para checar si faltaba un parametro ya que chocaba con la prueba 4

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -1,22 +1,22 @@
-const User = require('./../services/UserService')
+const UserService = require('./../services/UserService')
 
 class UserView{
 
     static createUser(payload){
         var objMessage = {}
+       
         if (payload===null){ 
-            objMessage = {error:"payload no existe"}
+            return objMessage = {error:"payload no existe"}
         }
-        else if(payload.username===null || payload.name===null || payload.id===null){
-            objMessage = {error:"necesitan tener un valor válido"}
+        else if(typeof payload.username !== 'string' || typeof payload.name !=='string' || typeof payload.id !=='number'){
+            return objMessage = {error:"necesitan tener un valor válido"}
         }
-        else if(payload.username===null || payload.name===null || payload.id===null){
-            objMessage = {error:"necesitan tener un valor válido"}
+        else if(payload.id === undefined || payload.username  === undefined || payload.name  === undefined){
+            return objMessage = {error:"necesitan tener un valor válido"}
         }
-        else if(payload.hasOwnProperty("username") || payload.hasOwnProperty("name")  || payload.hasOwnProperty("id") ){
-            objMessage = {error:"necesitan tener un valor válido"}
+        else {        
+            return UserService.create(payload.id, payload.username, payload.name)
         }
-        return objMessage
     }
 }
 
